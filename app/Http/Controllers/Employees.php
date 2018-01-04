a7ecb01822cc70b90013c0baeb9bc50e55c14078
pilip <pilipenko.andrey.79@gmail.com>
2018-01-04 10:58:31 +0200

    rest api

--- app/Http/Controllers/Employees.php
+++ app/Http/Controllers/Employees.php
@@ -4,91 +4,58 @@ namespace App\Http\Controllers;
 
 use App\Employee;
 use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class Employees extends Controller
 {
-
     /**
-     * Display a listing of the resource.
-     *
-     * @return Response
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
      */
-    public function index($id = null)
+    public function index()
     {
-        if ($id === null)
-        {
-            return Employee::orderBy('id', 'asc')->get();
-        }
-        else
-        {
-            return $this->show($id);
-        }
+        return Employee::all();
+    }
+    
+    /**
+     * @param Employee $employee
+     * @return Employee
+     */
+    public function show(Employee $employee)
+    {
+        return $employee;
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
+     * @param Request $request
+     * @return string
      */
     public function store(Request $request)
     {
-        $employee = new Employee;
-
-        $employee->name = $request->input('name');
-        $employee->email = $request->input('email');
-        $employee->contact_number = $request->input('contact_number');
-        $employee->position = $request->input('position');
-        $employee->save();
-
-        return 'Employee record successfully created with id ' . $employee->id;
+        $product = Product::create($request->all());
+ 
+        return response()->json($product, 201);
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
+     * @param Request $request
+     * @param \App\Http\Controllers\Product $product
+     * @return string
      */
-    public function show($id)
+    public function update(Request $request, Product $product)
     {
-        return Employee::find($id);
+        $product->update($request->all());
+ 
+        return response()->json($product, 200);
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
+     * @param \App\Http\Controllers\Product $product
+     * @return string
      */
-    public function update(Request $request, $id)
+    public function delete(Product $product)
     {
-        $employee = Employee::find($id);
-
-        $employee->name = $request->input('name');
-        $employee->email = $request->input('email');
-        $employee->contact_number = $request->input('contact_number');
-        $employee->position = $request->input('position');
-        $employee->save();
-
-        return "Sucess updating user #" . $employee->id;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Request $request
-     * @return Response
-     */
-    public function destroy(Request $request)
-    {
-        $employee = Employee::find($request->input('id'));
-
-        $employee->delete();
-
-        return "Employee record successfully deleted #" . $request->input('id');
+        $product->delete();
+ 
+        return response()->json(null, 204);
     }
 } 
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+** Basic Routes for a RESTful service:
+**
+** Route::get($uri, $callback);
+** Route::post($uri, $callback);
+** Route::put($uri, $callback);
+** Route::delete($uri, $callback);
+**
+**/
+ 
+Route::get('v1/employees', 'Employees@index');
+Route::get('v1/employees/{employee}', 'Employees@show');
+Route::post('v1/employees','Employees@store');
+Route::put('v1/employees/{employee}','Employees@update');
+Route::delete('v1/employees/{employee}', 'Employees@delete');
